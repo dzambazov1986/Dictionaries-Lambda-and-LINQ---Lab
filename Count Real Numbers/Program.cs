fb86d87fb4b1aced1d601f4475ad392c6d2f301f
Dimityr <dzambazov1986@gmail.com>
2024-03-11 21:21:46 +0200

    Add project files.

---
+++ Count Real Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Xml.Linq;
+
+List<double> numbers = Console.ReadLine()
+.Split()
+.Select(double.Parse)
+.ToList();
+
+SortedDictionary<double, int> counter = new SortedDictionary<double, int>();
+
+foreach (var number in numbers)
+{
+    if (counter.ContainsKey(number))
+    {
+        counter[number]++;
+    }
+    else
+    {
+        counter.Add(number, 1);
+    }
+}
+foreach (KeyValuePair<double, int> element in counter)
+{
+    Console.WriteLine($"{element.Key} -> {element.Value}");
+}
---
+++ Word Filter/Program.cs
@@ -0,0 +1,5 @@
+﻿var strings = Console.ReadLine()
+.Split()
+.Where(s => s.Length % 2 == 0)
+.Select(s => { Console.WriteLine(s); return s; })
+.ToList();
